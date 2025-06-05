822965c9d234e39911f55e045767b0ae370d371f
achensetyawati <achen.setyawati@gmail.com>
2025-06-05 09:20:50 +0700

    update transit items

--- src/modules/inventory/transit-warehouse/transit-warehouse-out/templates/item-header.js
+++ src/modules/inventory/transit-warehouse/transit-warehouse-out/templates/item-header.js
@@ -3,7 +3,7 @@ export class ItemHeader {
   activate(context) {
     this.context = context;
     this.data = context.data;
-    this.Items = context.Items;
+    this.Items = context.items;
     this.options = context.options;
     this.readOnly = this.options.readOnly;
   }
