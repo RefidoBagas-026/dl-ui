14adefa60119aa3e984e6c149a3a265e5fdc1a22
Refido Bagas Prakosa <refidobagas2607@gmail.com>
2026-01-09 15:24:20 +0700

    Enhance Master Tarif ( DMS ) UI

--- src/modules/garment-production/comodity-price/list.js
+++ src/modules/garment-production/comodity-price/list.js
@@ -1,6 +1,7 @@
 import { inject } from 'aurelia-framework';
 import { Service } from "./service";
 import { Router } from 'aurelia-router';
+import { filter } from 'bluebird';
 var moment = require("moment");
 
 @inject(Router, Service)
@@ -9,15 +10,23 @@ export class List {
         this.service = service;
         this.router = router;
     }
-
-    context = ["Rincian"];
    
     columns = [
         { field: "UnitCode", title: "Unit" },
         { field: "ComodityName", title: "Komoditi" },
-        { field: "Price", title: "Tarif" }
+        { field: "Price", title: "Tarif" },
+        { field: "IsApproved", title: "Status Approval" , formatter: function(value, data, index) {
+            return value ? "APPROVED" : "BELUM APPROVED";
+        } }
     ]
 
+    rowFormatter(data, index) {
+        if (data.IsApproved) {
+          return { classes: "success" };
+        }else{
+          return { css: {"background-color": "#fff"} };
+        }
+    }
     loader = (info) => {
         var order = {};
         if (info.sort)
@@ -27,12 +36,15 @@ export class List {
             page: parseInt(info.offset / info.limit, 10) + 1,
             size: info.limit,
             keyword: info.search,
-            order: order
+            order: order,
+            // filter: JSON.stringify({IsApproved: false})
         }
 
         return this.service.search(arg)
             .then(result => {
-
+                result.data.sort((a, b) => {
+                    return (b.IsApproved === false) - (a.IsApproved === false);
+                });
                 for(var a of result.data){
                     a.UnitCode=a.Unit.Code;
                     a.ComodityName=a.Comodity.Name;
