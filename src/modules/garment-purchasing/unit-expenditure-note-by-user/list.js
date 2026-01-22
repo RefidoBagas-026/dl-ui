404bed1972e8e0c978995c34c10a3dcedb1bc004
Refido Bagas Prakosa <refidobagas2607@gmail.com>
2026-01-22 08:57:17 +0700

    Fix Get Data Unit Expenditure Note

--- src/modules/garment-purchasing/unit-expenditure-note-by-user/list.js
+++ src/modules/garment-purchasing/unit-expenditure-note-by-user/list.js
@@ -36,13 +36,10 @@ export class List {
         
         return this.service.search(arg)
             .then(result => {
-                // var data = {};
-                // data.total = result.info.total;
-                // data.data = result.data;
-                let filteredData = result.data.filter(x => x.ExpenditureType !== "PROSES");
-                let total = filteredData.length;
-
-                filteredData.forEach(s => {
+                var data = {};
+                data.total = result.info.total;
+                data.data = result.data;
+                 data.data.forEach(s => {
                     s.toString = function () {
                         var str = "<ul>";
                         for (var item of s.Items) {
@@ -53,8 +50,8 @@ export class List {
                     }
                 });
                 return {
-                    total: total,
-                    data: filteredData
+                     total: result.info.total,
+                     data: result.data
                 }
             });
     }
