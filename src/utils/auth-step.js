30129e231cf1be94a03f8cd48cde40102160be29
Refido Bagas Prakosa <refidobagas2607@gmail.com>
2026-01-23 10:24:03 +0700

    Fix Core Menu Auth Permission

--- src/utils/auth-step.js
+++ src/utils/auth-step.js
@@ -14,47 +14,25 @@ export class AuthStep {
         const loginRoute = this.authService.config.loginRoute;
         const config = this.authService.authentication.config;
         const storage = this.authService.authentication.storage;
-        const forbiddenRoute = "/";
+        const forbiddenRoute = "/forbidden";
 
         if (routingContext.getAllInstructions().some(route => route.config.auth === true)) {
             if (!isLoggedIn) {
                 return next.cancel(new Redirect(loginRoute));
             }
-            else {
-                // Check permission for the route
-                const hasNoPermission = routingContext.getAllInstructions().some(route => {
-                    const token = JSON.parse(storage.get(config.storageKey));
-                    var user = jwtDecode(token.data);
-                    var routeSettings = route.config.settings || {};
-                    var routePermission = routeSettings.permission || {}; // permission is an object like { H70: 1 }
-                    // Parse user permissions (stored as JSON string in token)
-                    var userPermissions = {};
-                    try {
-                        userPermissions = user.permission ? JSON.parse(user.permission) : {};
-                    } catch (e) {
-                        userPermissions = user.permission || {};
-                    }
+            else if (isLoggedIn && routingContext.getAllInstructions().some(route => {
+                const token = JSON.parse(storage.get(config.storageKey));
+                var user = jwtDecode(token.data);
+                var routeSettings = route.config.settings || {};
+                var routePermissions = routeSettings.permissions || [];
+                var userPermissions = user.permissions;
 
-                    // If route has no permission setting, allow access
-                    if (Object.keys(routePermission).length === 0) {
-                        return false;
-                    }
-
-                    // Check if user has the required permission
-                    // routePermission format: { "H70": 1 } where key is permission code and value is minimum level
-                    for (const [permCode, requiredLevel] of Object.entries(routePermission)) {
-                        const userLevel = userPermissions[permCode] || 0;
-                        if (userLevel < requiredLevel) {
-                            return true; // User doesn't have permission
-                        }
-                    }
-
-                    return false; // User has permission
+                return routePermissions.some(permission => {
+                    return false;
+                    //userPermissions.indexOf(permission)
                 });
-
-                if (hasNoPermission) {
-                    return next.cancel(new Redirect(forbiddenRoute));
-                }
+            })) {
+                return next.cancel(new Redirect(forbiddenRoute));
             }
         }
         else if (isLoggedIn && routingContext.getAllInstructions().some(route => [loginRoute].indexOf(route.fragment) >= 0)) {
